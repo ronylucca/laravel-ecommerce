de6a07ec7994b3ea8f879cca614df16ca634932b
Rony de Lucca <tc036746@oi.corp.net>
2017-08-31 15:16:23 -0300

    Incluido confguração do laravel-mix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,30 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// /**
+//  * First we will load all of this project's JavaScript dependencies which
+//  * includes Vue and other libraries. It is a great starting point when
+//  * building robust, powerful web applications using Vue and Laravel.
+//  */
 
-require('./bootstrap');
+// require('./bootstrap');
 
-window.Vue = require('vue');
+// window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+// /**
+//  * Next, we will create a fresh Vue application instance and attach it to
+//  * the page. Then, you may begin adding components to this application
+//  * or customize the JavaScript scaffolding to fit your unique needs.
+//  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
+require('../../../node_modules/jquery/dist/jquery.min')
+require('../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min')
+
+require('./move-top')
+require('./easing')
+require('../../../node_modules/flexslider/jquery.flexslider-min')
+require('./minicart.min')
+require('./site')
